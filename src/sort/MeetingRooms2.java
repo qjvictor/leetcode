1100ba2614a150c32eb0e4b98cb84ade4595d4d4
victor.qian <qjvictor@gmail.com>
2015-10-04 01:35:31 -0400

    one more solution for meeting room2

--- src/sort/MeetingRooms2.java
+++ src/sort/MeetingRooms2.java
@@ -33,18 +33,52 @@ public class MeetingRooms2 {
         }
         return pq.size();
     }
+	/**
+	 * # Very similar with what we do in real life. Whenever you want to start a meeting,
+		# you go and check if any empty room available (available > 0) and
+		# if so take one of them ( available -=1 ). Otherwise,
+		# you need to find a new room someplace else ( numRooms += 1 ). 
+		# After you finish the meeting, the room becomes available again ( available += 1 ). 
+	 * @param intervals
+	 * @return
+	 */
+	public int minMeetingRooms2(Interval[] intervals) {  
+        if (intervals == null || intervals.length == 0)  
+            return 0;  
+        int N = intervals.length;  
+        int[] starts = new int[N];  
+        int[] ends = new int[N];  
+        for (int i = 0; i < intervals.length; i++) {  
+            starts[i] = intervals[i].start;  
+            ends[i] = intervals[i].end;  
+        }  
+        Arrays.sort(starts);  
+        Arrays.sort(ends);  
+        int e = 0, rooms = 0, available = 0;  
+        for (int start : starts) {  
+            while (ends[e] <= start) {  //free the finished meetings.
+                available++;  
+                e++;  
+            }  
+            if (available > 0)  
+                available--;  
+            else  
+                rooms++;  
+        }  
+        return rooms;  
+    }  
 	
 	public static void main(String[] args){
 		MeetingRooms2 m = new MeetingRooms2();
 		Interval i1=new Interval(4,9);
 		Interval i2=new Interval(4,17);
 		Interval i3=new Interval(9,10);
-		System.out.println(m.minMeetingRooms(new Interval[]{i1,i2,i3}));
+		System.out.println(m.minMeetingRooms2(new Interval[]{i1,i2,i3}));
 		
 		 i1=new Interval(4,10);
 		 i2=new Interval(13,20);
 		 i3=new Interval(10,15);
 		 Interval i4=new Interval(16,19);
-		System.out.println(m.minMeetingRooms(new Interval[]{i1,i2,i3, i4}));
+		System.out.println(m.minMeetingRooms2(new Interval[]{i1,i2,i3, i4}));
 	}
 }
