f8ad96e8c80c6e5b9574226554b1ee054dbc3ce0
victor.qian <qjvictor@gmail.com>
2015-10-24 01:48:11 -0400

    NONE

---
+++ src/mathandbit/ReverseBits.java
@@ -0,0 +1,31 @@
+package mathandbit;
+
+/**
+ * Reverse bits of a given 32 bits unsigned integer.
+ * 
+ * For example, given input 43261596 (represented in binary as
+ * 00000010100101000001111010011100), return 964176192 (represented in binary as
+ * 00111001011110000010100101000000).
+ * 
+ * Follow up: If this function is called many times, how would you optimize it?
+ * 
+ * @author VictorQian
+ * 
+ */
+public class ReverseBits {
+	// you need treat n as an unsigned value
+    public int reverseBits(int n) {
+		int ret = 0;
+		for (int i = 0; i < 32; i++) {
+			int t = n & (1 << i);
+			ret = (ret << 1) + ((t == 0) ? 0 : 1);
+			i++;
+		}
+		return ret;
+    }
+    
+    public static void main(String[] args){
+    	ReverseBits r = new ReverseBits();
+    	System.out.println(r.reverseBits(43261596));
+    }
+}
