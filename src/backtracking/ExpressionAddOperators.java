879372dd541b8d950aa3dd0d7ee524b1e4623452
victor.qian <qjvictor@gmail.com>
2015-11-03 23:44:43 -0500

    NONE

--- src/backtracking/ExpressionAddOperators.java
+++ src/backtracking/ExpressionAddOperators.java
@@ -21,13 +21,35 @@ Examples:
  */
 public class ExpressionAddOperators {
 	public List<String> addOperators(String num, int target) {
-        List<String> ret = new ArrayList<String>();
-        if(num==null || num.length()==0) return ret;
-        helper(ret, num, target);
-        return ret;
-    }
+		List<String> rst = new ArrayList<String>();
+		if (num == null || num.length() == 0)
+			return rst;
+		helper(rst, "", num, target, 0, 0, 0);
+		return rst;
+	}
+
+	public void helper(List<String> rst, String path, String num, int target, int pos, long sum, long lastNum) {
+		if (pos == num.length()) {
+			if (target == sum)
+				rst.add(path);
+			return;
+		}
+		for (int i = pos; i < num.length(); i++) {
+			if (i != pos && num.charAt(pos) == '0')
+				break;// ignore the number starting with 0
+			long cur = Long.parseLong(num.substring(pos, i + 1));
+			if (pos == 0) {
+				helper(rst, cur + "", num, target, i + 1, cur, cur);
+			} else {
+				helper(rst, path + "+" + cur, num, target, i + 1, sum + cur, cur);
+				helper(rst, path + "-" + cur, num, target, i + 1, sum - cur, -cur);
+				helper(rst, path + "*" + cur, num, target, i + 1, sum - lastNum + lastNum * cur, lastNum * cur);// previous sum - lastNum+lastNum*cur.
+			}
+		}
+	}
 	
-	private void helper(List<String> ret, String num, int target){
-		
+	public static void main(String[] args){
+		ExpressionAddOperators e = new ExpressionAddOperators();
+		System.out.println(e.addOperators("105", 5));
 	}
 }
